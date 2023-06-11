1f518903e249ca7fc9ad62f68c97966a7f72ff46
da314jones <dwaynejones@pursuit.org>
2023-06-10 22:57:05 -0400

    working on switch statements and and price total

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {
     createNewData,
     destroyPuppetInfo,
     showPuppetInfo,
+    puppetIndices,
     puppetUpdate,
     } = require("./popIndex");
 
@@ -10,9 +11,27 @@ const run = () => {
     const puppet = process.argv[3];
     let puppets = readJSONFile('./data', 'puppet_data.json')
     let writeToFile = false;
-    let updatedPuppet = [];
+    let updatedPuppets = [];
 
     switch (action) {
-        case 
+        case "index":
+            const allPuppets = puppetIndices(puppets)
+            console.log(allPuppets);
+            break;
+        case "createNewData":
+            console.log("Create is firing!")
+            updatedPuppets = create(puppets, puppet)
+            writeToFile = tre;
+            break;
+        case "show":
+            console.log(puppets, " $$$$$$ ")
+            updatedPuppets = update(puppets, puppet, process.argv[4]);
+            writeToFile = true;
+            break;
+        case "destroy":
+            updatedPuppets = destroy(puppets, puppet);
+            writeToFile = true;
+            break;
+        case
     }
 }
